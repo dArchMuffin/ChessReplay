d76fea6d6c1f8ba7c1ed8ba85558c5d744ddee5e
LeekScriptEnthusiast <olivier.hellome@gmail.com>
2024-12-27 00:19:21 +0100

    cleaners division & opti

---
+++ src/clean _diags.c
@@ -0,0 +1,96 @@
+#include "header.h"
+#include <stdio.h>
+
+int	clean_diag_top(char p, char y, char x, int board[8][8])
+{
+	char	start_y;
+	char	start_x;
+
+	start_y = y;
+	start_x = x;
+	while (y < 7 && x < 7)
+	{
+		y++;
+		x++;
+		// printf("diag top right : board[%d][%d] = %c\n", x, y, board[x][y]);
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	y = start_y;
+	x = start_x;
+	while (y > 0 && x < 7)
+	{
+		y--;
+		x++;
+		// printf("diag top left : board[%d][%d] = %c\n", x, y, board[x][y]);
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	clean_diag_bot(char p, char y, char x, int board[8][8])
+{
+	char	start_y;
+	char	start_x;
+
+	start_y = y;
+	start_x = x;
+	// diag bot left
+	while (y > 0 && x > 0)
+	{
+		y--;
+		x--;
+		// printf("diag bot left : board[%d][%d] = %c\n", x, y, board[x][y]);
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	y = start_y;
+	x = start_x;
+	while (y < 7 && x > 0)
+	{
+		y++;
+		x--;
+		// printf("diag bot right : board[%d][%d] = %c\n", x, y, board[x][y]);
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	clean_piece_in_diags(char p, char start_y, char start_x, int board[8][8],
+							int move_idx) // A racourcir !
+{
+	int y;
+	int x;
+
+	y = start_y - 'a';
+	x = start_x - '0' - 1;
+	if (move_idx % 2 != 0)
+		p -= 32;
+	if (clean_diag_bot(p, y, x, board) && clean_diag_top(p, y, x, board))
+		return(1);
+	else 
+		return(0);
+	// printf("%c not found in diag\n", p);
+}
---
+++ src/clean_lines.c
@@ -0,0 +1,87 @@
+#include "header.h"
+#include <stdio.h>
+
+int	clean_line_col(char p, char y, char x, int board[8][8])
+{
+	char	start_x;
+
+	start_x = x;
+	// col up
+	while (x < 7)
+	{
+		x++;
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	// col down
+	x = start_x;
+	while (x > 0)
+	{
+		x--;
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	clean_line_row(char p, char y, char x, int board[8][8])
+{
+	char	start_y;
+
+	start_y = y;
+	// in row left
+	while (y > 0)
+	{
+		y--;
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	y = start_y;
+	// in row right
+	while (y < 7)
+	{
+		y++;
+		if (board[x][y] != ' ' && board[x][y] != p)
+			break ;
+		if (board[x][y] == p)
+		{
+			board[x][y] = ' ';
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	clean_piece_in_lines(char p, char start_y, char start_x, int board[8][8],
+		int move_idx)
+{
+	int	y;
+	int	x;
+
+	y = start_y - 'a';
+	x = start_x - '0' - 1;
+	// printf("x = %d | y = %d\n", x, y);
+	if (move_idx % 2 != 0)
+		p -= 32;
+	if (clean_line_col(p, y, x, board) && clean_line_row(p, y, x, board))
+		return (1);
+	else
+		return (0);
+	// printf("%c not found in lines\n", p);
+	return (1);
+}
