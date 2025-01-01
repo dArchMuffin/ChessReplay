6d0c525d65519596943afde886711e9d518f6746
LeekScriptEnthusiast <olivier.hellome@gmail.com>
2025-01-01 21:49:50 +0100

    header cleaned

--- include/header.h
+++ include/header.h
@@ -13,7 +13,7 @@ typedef enum
 	ROOK,
 	QUEEN,
 	KING
-}			piece_type;
+}				piece_type;
 
 typedef enum
 {
@@ -21,81 +21,81 @@ typedef enum
 	SHORT_CASTLE,
 	LONG_CASTLE,
 	CAPTURE
-}			move_type;
+}				move_type;
 
-typedef	enum
+typedef enum
 {
 	WHITE_WIN,
 	BLACK_WIN,
 	DRAW,
 	PNEDING,
-}			result;
+}				result;
 
 typedef struct
 {
 	piece_type piece; // quelle piece ?
 	move_type type;   // x ou deplacement ou speciaux
-	result	res;
-	char	*pgn;
-	char	*eval;
-	char	comment[3];
-	char	destination[3];
-	char	col_hint;
-	char	row_hint;
-	bool	is_check;
-	bool	is_mate;
-	bool	is_draw;
-	bool	is_resign;
-	bool	is_time_out;
-	bool	is_promotion;
+	result		res;
+	char		*pgn;
+	char		*eval;
+	char		comment[3];
+	char		destination[3];
+	char		col_hint;
+	char		row_hint;
+	bool		is_check;
+	bool		is_mate;
+	bool		is_draw;
+	bool		is_resign;
+	bool		is_time_out;
+	bool		is_promotion;
 
-	piece_type promoted;
-}			move;
+	piece_type	promoted;
+}				move;
 
 typedef struct
 {
-	char	**headers;
-	int		nb_headers;
-	move	*moves;
-	int		nb_moves;
-}			game_info;
+	char		**headers;
+	int			nb_headers;
+	move		*moves;
+	int			nb_moves;
+}				game_info;
 
 // libft
-char		*ft_strchr(const char *s, int c);
-size_t		ft_strlen(const char *s);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		*ft_substr(char const *s, unsigned int start, size_t len);
-int			ft_isdigit(int c);
-int			ft_isalpha(int c);
-int			ft_islowercase(int c);
+char			*ft_strchr(const char *s, int c);
+size_t			ft_strlen(const char *s);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+int				ft_isdigit(int c);
+int				ft_isalpha(int c);
+int				ft_islowercase(int c);
 
 // Chess
 // Parsing
-int			read_game(int game_number, game_info *infos);
-void		free_game_info(game_info *game_info);
-move	parse_moves(char *game, int i);
+int				read_game(int game_number, game_info *infos);
+void			free_game_info(game_info *game_info);
+move			parse_moves(char *game, int i);
 
 // Board
-void		init_board(int board[8][8]);
-void		print_board(int board[8][8], game_info *infos);
+void			init_board(int board[8][8]);
+void			print_board(int board[8][8], game_info *infos);
 // write_move
-int			*write_move(game_info *infos, int board[8][8], int move_idx);
+int				*write_move(game_info *infos, int board[8][8], int move_idx);
 // update_board
-void		update_board(game_info *infos, int board[8][8], int move_idx);
-void		no_hint_case(game_info *infos, int board[8][8], int move_idx);
-void		col_hint_case(game_info *infos, int board[8][8], int move_idx);
-void		row_hint_case(game_info *infos, int board[8][8], int move_idx);
+void			update_board(game_info *infos, int board[8][8], int move_idx);
+void			no_hint_case(game_info *infos, int board[8][8], int move_idx);
+void			col_hint_case(game_info *infos, int board[8][8], int move_idx);
+void			row_hint_case(game_info *infos, int board[8][8], int move_idx);
 // cleaners
-void		clean_origin(game_info *infos, int board[8][8], int move_idx);
-int			clean_knight(char p, char start_y, char start_x, int board[8][8],
-				int move_idx);
-int			clean_piece_in_lines(char p, char start_y, char start_x,
-				int board[8][8], int move_idx);
+void			clean_origin(game_info *infos, int board[8][8], int move_idx);
+int				clean_knight(char p, char start_y, char start_x,
+					int board[8][8], int move_idx);
+int				clean_piece_in_lines(char p, char start_y, char start_x,
+					int board[8][8], int move_idx);
 int	clean_piece_in_diags(char p, char start_y, char start_x, int board[8][8],
-							int move_idx); // A racourcir !
-int			clean_king(char p, char start_y, char start_x, int board[8][8],
-				int move_idx);
+							int move_idx);
+int				clean_king(char p, char start_y, char start_x, int board[8][8],
+					int move_idx);
 
-int 		endgame_check(game_info *infos, int move_idx);
+int				endgame_check(game_info *infos, int move_idx);
 
 #endif
